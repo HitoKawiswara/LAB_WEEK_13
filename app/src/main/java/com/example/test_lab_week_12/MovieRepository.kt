8cd0463f5e5edbb579a58e0984f000ccc78372fc
HitoKawiswara <hitokawiswara@gmail.com>
2025-12-09 17:46:20 +0700

    initial commit

---
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -0,0 +1,17 @@
+package com.example.test_lab_week_12
+
+import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "ca47ccde3b68120c92b5f0372fc87e85"
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
+    }
+}
---
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.test_lab_week_12
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+class MovieViewModel(
+    private val movieRepository: MovieRepository
+) : ViewModel() {
+
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
+    init {
+        // supaya langsung load saat ViewModel dibuat
+        fetchPopularMovies()
+    }
+
+    private fun fetchPopularMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies()
+                .catch { e ->
+                    _error.value = "An exception occurred: ${e.message}"
+                }
+                .collect { movies ->
+                    // ✅ apply filter: urutkan descending berdasarkan popularity
+                    _popularMovies.value = movies
+                        .sortedByDescending { it.popularity }
+                }
+        }
+    }
+}
