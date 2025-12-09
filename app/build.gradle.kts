3c248483a2773439bc4c97f3634678122d6e8252
HitoKawiswara <hitokawiswara@gmail.com>
2025-12-09 18:26:37 +0700

    Commit No. 2 – update to Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,11 @@ android {
 }
 
 dependencies {
+    //room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
     // Moshi core + codegen
     implementation("com.squareup.moshi:moshi:1.15.2")
     //noinspection KaptUsageInsteadOfKsp
--- app/src/main/java/com/example/lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -1,17 +1,43 @@
 package com.example.lab_week_13
 
 import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.database.MovieDao
 import com.example.lab_week_13.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class MovieRepository(private val movieService: MovieService) {
-    private val apiKey = "ca47ccde3b68120c92b5f0372fc87e85"
+class MovieRepository(
+    private val movieService: MovieService,
+    private val movieDao: MovieDao
+) {
+
+    // API key kamu
+    private val apiKey = "4ebb92bb9df6dd74c5c162ab29810a81"
+
+    // fetch movies from the API / DB
+    // 1. cek DB dulu
+    // 2. kalau kosong → fetch dari API, simpan ke DB, lalu emit
+    // 3. kalau ada → langsung emit dari DB
     fun fetchMovies(): Flow<List<Movie>> {
         return flow {
-            emit(movieService.getPopularMovies(apiKey).results)
+            // Ambil data dari DB dulu
+            val savedMovies = movieDao.getMovies()
+
+            if (savedMovies.isEmpty()) {
+                // Tidak ada di DB → fetch dari API
+                val movies = movieService.getPopularMovies(apiKey).results
+
+                // Simpan hasil API ke DB
+                movieDao.addMovies(movies)
+
+                // Emit data dari API
+                emit(movies)
+            } else {
+                // Ada di DB → langsung emit dari DB
+                emit(savedMovies)
+            }
         }.flowOn(Dispatchers.IO)
     }
 }
